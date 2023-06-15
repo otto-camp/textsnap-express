1b1336c7adb2d166c4313548a83255a6643b0f2c
İsmail Yarar <ismailyarar4@gmail.com>
2023-06-15 19:30:09 +0300

    Add multiple parallel HTTP requests.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
 import multer from "multer";
+import cluster from "cluster";
+import os from "os";
 
 import { getTextFromImageFile } from "./services/getTextFromImageFile";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -17,18 +19,34 @@ const port = (process.env.PORT as any) || 3000;
 app.use(express.json({ limit: "10mb" }));
 app.use(cors(), bodyParser.json());
 
-app.listen(port, "0.0.0.0", () =>
-  console.log("Server listening on port", port)
-);
+if (cluster.isMaster) {
+  const numWorkers = os.cpus().length;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
-});
+  console.log(`Master cluster setting up ${numWorkers} workers...`);
 
-app.post(
-  "/:lang/image",
-  upload.single("image"),
-  (req: Request, res: Response) => {
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("online", (worker) => {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`);
+    console.log("Starting a new worker...");
+    cluster.fork();
+  });
+} else {
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Worker ${process.pid} listening on port ${port}`);
+  });
+
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World");
+  });
+
+  app.post("/:lang/image", upload.single("image"), (req: Request, res: Response) => {
     if (req.file) {
       getTextFromImageFile(req.file.path, req.params.lang)
         .then((text) => {
@@ -40,21 +58,21 @@ app.post(
     } else {
       res.status(404).send("file is broken.");
     }
-  }
-);
+  });
 
-app.post("/:lang/image-url", (req: Request, res: Response) => {
-  if (req.body.url) {
-    getTextFromImageUrl(req.body.url, req.params.lang)
-      .then((text) => {
-        res.send(text);
-      })
-      .catch((error) => {
-        res.status(404).send(error);
-      });
-  } else {
-    res.status(404).send("file is broken.");
-  }
-});
+  app.post("/:lang/image-url", (req: Request, res: Response) => {
+    if (req.body.url) {
+      getTextFromImageUrl(req.body.url, req.params.lang)
+        .then((text) => {
+          res.send(text);
+        })
+        .catch((error) => {
+          res.status(404).send(error);
+        });
+    } else {
+      res.status(404).send("file is broken.");
+    }
+  });
 
-app.use(errorHandler);
+  app.use(errorHandler);
+}
