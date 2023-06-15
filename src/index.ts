4acab2bba92a519f944b7e8777f7cf329f999de9
İsmail Yarar <ismailyarar4@gmail.com>
2023-06-15 19:53:58 +0300

    Change cluster with socket.io

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
 import multer from "multer";
-import cluster from "cluster";
-import os from "os";
+import http from "http";
+import { Server, Socket } from "socket.io";
 
 import { getTextFromImageFile } from "./services/getTextFromImageFile";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -14,65 +14,87 @@ dotenv.config();
 
 const upload = multer({ dest: "uploads/" });
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = (process.env.PORT as any) || 3000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cors(), bodyParser.json());
 
-if (cluster.isMaster) {
-  const numWorkers = os.cpus().length;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
 
-  console.log(`Master cluster setting up ${numWorkers} workers...`);
-
-  for (let i = 0; i < 2; i++) {
-    cluster.fork();
+app.post("/:lang/image", upload.single("image"), (req: Request, res: Response) => {
+  if (req.file) {
+    getTextFromImageFile(req.file.path, req.params.lang)
+      .then((text) => {
+        res.send(text);
+      })
+      .catch((error) => {
+        res.status(404).send(error);
+      });
+  } else {
+    res.status(404).send("file is broken.");
   }
+});
 
-  cluster.on("online", (worker) => {
-    console.log(`Worker ${worker.process.pid} is online`);
-  });
+app.post("/:lang/image-url", (req: Request, res: Response) => {
+  if (req.body.url) {
+    getTextFromImageUrl(req.body.url, req.params.lang)
+      .then((text) => {
+        res.send(text);
+      })
+      .catch((error) => {
+        res.status(404).send(error);
+      });
+  } else {
+    res.status(404).send("file is broken.");
+  }
+});
 
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`);
-    console.log("Starting a new worker...");
-    cluster.fork();
-  });
-} else {
-  app.listen(port, "0.0.0.0", () => {
-    console.log(`Worker ${process.pid} listening on port ${port}`);
-  });
+app.use(errorHandler);
 
-  app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
-  });
+io.on("connection", (socket: Socket) => {
+  console.log("A client connected");
 
-  app.post("/:lang/image", upload.single("image"), (req: Request, res: Response) => {
-    if (req.file) {
-      getTextFromImageFile(req.file.path, req.params.lang)
+  socket.on("imageRequest", (data) => {
+    const { lang, file } = data;
+    
+    if (file) {
+      getTextFromImageFile(file, lang)
         .then((text) => {
-          res.send(text);
+          socket.emit("imageResponse", text);
         })
         .catch((error) => {
-          res.status(404).send(error);
+          socket.emit("imageError", error);
         });
     } else {
-      res.status(404).send("file is broken.");
+      socket.emit("imageError", "file is broken.");
     }
   });
 
-  app.post("/:lang/image-url", (req: Request, res: Response) => {
-    if (req.body.url) {
-      getTextFromImageUrl(req.body.url, req.params.lang)
+  socket.on("imageUrlRequest", (data) => {
+    const { lang, url } = data;
+    
+    if (url) {
+      getTextFromImageUrl(url, lang)
         .then((text) => {
-          res.send(text);
+          socket.emit("imageUrlResponse", text);
         })
         .catch((error) => {
-          res.status(404).send(error);
+          socket.emit("imageUrlError", error);
         });
     } else {
-      res.status(404).send("file is broken.");
+      socket.emit("imageUrlError", "url is broken.");
     }
   });
 
-  app.use(errorHandler);
-}
+  socket.on("disconnect", () => {
+    console.log("A client disconnected");
+  });
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("Server listening on port", port);
+});
