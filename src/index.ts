1c0e29ad5195f671d9a9c774e3fca9fa02165a1b
İsmail Yarar <ismailyarar4@gmail.com>
2023-06-16 10:56:23 +0300

    Remove socket.io. Add async param to api endpoints.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import bodyParser from "body-parser";
 import multer from "multer";
-import http from "http";
-import { Server, Socket } from "socket.io";
 
 import { getTextFromImageFile } from "./services/getTextFromImageFile";
 import { errorHandler } from "./middlewares/errorHandler";
@@ -14,34 +12,40 @@ dotenv.config();
 
 const upload = multer({ dest: "uploads/" });
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 const port = (process.env.PORT as any) || 3000;
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cors(), bodyParser.json());
 
+app.listen(port, "0.0.0.0", () =>
+  console.log("Server listening on port", port)
+);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.post("/:lang/image", upload.single("image"), (req: Request, res: Response) => {
-  if (req.file) {
-    getTextFromImageFile(req.file.path, req.params.lang)
-      .then((text) => {
-        res.send(text);
-      })
-      .catch((error) => {
-        res.status(404).send(error);
-      });
-  } else {
-    res.status(404).send("file is broken.");
+app.post(
+  "/:lang/image",
+  upload.single("image"),
+  async(req: Request, res: Response) => {
+    if (req.file) {
+      await getTextFromImageFile(req.file.path, req.params.lang)
+        .then((text) => {
+          res.send(text);
+        })
+        .catch((error) => {
+          res.status(404).send(error);
+        });
+    } else {
+      res.status(404).send("file is broken.");
+    }
   }
-});
+);
 
-app.post("/:lang/image-url", (req: Request, res: Response) => {
+app.post("/:lang/image-url", async(req: Request, res: Response) => {
   if (req.body.url) {
-    getTextFromImageUrl(req.body.url, req.params.lang)
+    await getTextFromImageUrl(req.body.url, req.params.lang)
       .then((text) => {
         res.send(text);
       })
@@ -54,47 +58,3 @@ app.post("/:lang/image-url", (req: Request, res: Response) => {
 });
 
 app.use(errorHandler);
-
-io.on("connection", (socket: Socket) => {
-  console.log("A client connected");
-
-  socket.on("imageRequest", (data) => {
-    const { lang, file } = data;
-    
-    if (file) {
-      getTextFromImageFile(file, lang)
-        .then((text) => {
-          socket.emit("imageResponse", text);
-        })
-        .catch((error) => {
-          socket.emit("imageError", error);
-        });
-    } else {
-      socket.emit("imageError", "file is broken.");
-    }
-  });
-
-  socket.on("imageUrlRequest", (data) => {
-    const { lang, url } = data;
-    
-    if (url) {
-      getTextFromImageUrl(url, lang)
-        .then((text) => {
-          socket.emit("imageUrlResponse", text);
-        })
-        .catch((error) => {
-          socket.emit("imageUrlError", error);
-        });
-    } else {
-      socket.emit("imageUrlError", "url is broken.");
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A client disconnected");
-  });
-});
-
-server.listen(port, "0.0.0.0", () => {
-  console.log("Server listening on port", port);
-});
